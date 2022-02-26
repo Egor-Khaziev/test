a2f6cb304cce070875b92e3e181bcd9ecf670c0d
Egor_Khaziev <egor5.87@mail.ru>
2022-02-26 23:14:00 +0600

    test

---
+++ test/src/main/java/com/ekhaziev/alfasoft/task/configs/AppConfig.java
@@ -0,0 +1,10 @@
+package com.ekhaziev.alfasoft.task.configs;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+//@PropertySource("secrets.properties")
+public class AppConfig {
+}
---
+++ test/src/main/java/com/ekhaziev/alfasoft/task/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ekhaziev.alfasoft.task.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
